e38bd2632531a8d722346f8e6b2e26cbce0ca6f0
gpmarchi <gustavomarchi@gmail.com>
2026-02-03 13:04:04 -0300

    feat: add restaurant relation to user

--- src/database/schema/restaurants.ts
+++ src/database/schema/restaurants.ts
@@ -1,10 +1,25 @@
 import { createId } from "@paralleldrive/cuid2";
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { usersTable } from "./users";
 
 export const restaurantsTable = pgTable('restaurants', {
   id: text('id').$defaultFn(() => createId()).primaryKey(),
   name: text('name').notNull(),
   description: text('description'),
+  managerId: text('manager_id').references(() => usersTable.id, {
+    onDelete: 'set null',
+  }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const restaurantsRelations = relations(restaurantsTable, ({one}) => {
+  return {
+    manager: one(usersTable, {
+      fields: [restaurantsTable.managerId],
+      references: [usersTable.id],
+      relationName: 'restaurant_manager',
+    })
+  }
 })
