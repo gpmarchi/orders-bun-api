b30ffa58b6e0e6a75b45e926cfdfa7875af61993
gpmarchi <gustavomarchi@gmail.com>
2026-02-04 10:53:45 -0300

    feat: validate request body using `zod`

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,29 +1,41 @@
 import { Elysia } from 'elysia'
+import z from 'zod'
 import { db } from '../database/connection'
 import { restaurantsTable, usersTable } from '../database/schema'
 
-const app = new Elysia().post('/restaurants', async ({ body, set }) => {
-  const { restaurantName, name, email, phone } = body as any
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
 
-  const [manager] = await db
-    .insert(usersTable)
-    .values({
-      name,
-      email,
-      phone,
-      role: 'manager',
+    const [manager] = await db
+      .insert(usersTable)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: 'manager',
+      })
+      .returning({
+        id: usersTable.id,
+      })
+
+    await db.insert(restaurantsTable).values({
+      name: restaurantName,
+      managerId: manager?.id,
     })
-    .returning({
-      id: usersTable.id,
-    })
 
-  await db.insert(restaurantsTable).values({
-    name: restaurantName,
-    managerId: manager?.id,
-  })
-
-  set.status = 204
-})
+    set.status = 204
+  },
+  {
+    body: z.object({
+      restaurantName: z.string(),
+      managerName: z.string(),
+      email: z.email(),
+      phone: z.string(),
+    }),
+  }
+)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
