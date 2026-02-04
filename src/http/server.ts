03504f7aff727bc522cb5861b78e92d49b558e41
gpmarchi <gustavomarchi@gmail.com>
2026-02-04 10:23:13 -0300

    feat: create restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,29 @@
 import { Elysia } from 'elysia'
+import { db } from '../database/connection'
+import { restaurantsTable, usersTable } from '../database/schema'
 
-const app = new Elysia().get('/', () => 'Hello World!')
+const app = new Elysia().post('/restaurants', async ({ body, set }) => {
+  const { restaurantName, name, email, phone } = body as any
+
+  const [manager] = await db
+    .insert(usersTable)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: usersTable.id,
+    })
+
+  await db.insert(restaurantsTable).values({
+    name: restaurantName,
+    managerId: manager?.id,
+  })
+
+  set.status = 204
+})
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
