36cade1587fd4d81f563ecfa8e40fab4a19cd62c
gpmarchi <gustavomarchi@gmail.com>
2026-02-05 13:01:20 -0300

    feat: send test email using `nodemailer`

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -1,9 +1,11 @@
 import { createId } from '@paralleldrive/cuid2'
 import Elysia from 'elysia'
+import nodemailer from 'nodemailer'
 import z from 'zod'
 import { db } from '../../database/connection'
 import { authLinksTable } from '../../database/schema'
 import { env } from '../../env'
+import { mail } from '../../lib/mail'
 
 export const sendAuthLink = new Elysia().post(
   '/authenticate',
@@ -32,14 +34,22 @@ export const sendAuthLink = new Elysia().post(
       code: authLinkCode,
     })
 
-    // TODO: Send email with the link containing the code
-
     const authLink = new URL('/auth-links/authenticate', env.API_BASE_URL)
 
     authLink.searchParams.set('code', authLinkCode)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
-    console.log(authLink.toString())
+    const message = await mail.sendMail({
+      from: {
+        name: 'Orders Bun API',
+        address: 'hi@ordersbun.com',
+      },
+      to: email,
+      subject: 'Authenticate to Orders Bun API',
+      text: `Use the following link to authenticate on Orders Bun API: ${authLink.toString()}`,
+    })
+
+    console.log(nodemailer.getTestMessageUrl(message))
   },
   {
     body: z.object({
