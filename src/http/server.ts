7b077bbcfabbc44f8ca3ef0152587731ab3c247c
gpmarchi <gustavomarchi@gmail.com>
2026-02-07 18:43:01 -0300

    feat: day total orders amount metric route

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import { authenticateFromLink } from './routes/authenticate-from-link'
 import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
+import { getDayTotalOrdersAmount } from './routes/get-day-total-orders-amount'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getMonthlyRevenue } from './routes/get-monthly-revenue'
 import { getOrderDetails } from './routes/get-order-details'
@@ -27,6 +28,7 @@ const app = new Elysia()
   .use(dispatchOrder)
   .use(getOrders)
   .use(getMonthlyRevenue)
+  .use(getDayTotalOrdersAmount)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION':
