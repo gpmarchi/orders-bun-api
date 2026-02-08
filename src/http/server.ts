8f59b8854b9001db518e0f082b2ed73535cd2f1d
gpmarchi <gustavomarchi@gmail.com>
2026-02-08 12:10:33 -0300

    feat: monthly canceled orders amount metric route

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getDayTotalOrdersAmount } from './routes/get-day-total-orders-amount'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getMonthlyCanceledOrdersAmount } from './routes/get-monthly-canceled-orders-amount'
 import { getMonthlyOrdersAmount } from './routes/get-monthly-orders-amount'
 import { getMonthlyRevenue } from './routes/get-monthly-revenue'
 import { getOrderDetails } from './routes/get-order-details'
@@ -31,6 +32,7 @@ const app = new Elysia()
   .use(getMonthlyRevenue)
   .use(getDayTotalOrdersAmount)
   .use(getMonthlyOrdersAmount)
+  .use(getMonthlyCanceledOrdersAmount)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION':
